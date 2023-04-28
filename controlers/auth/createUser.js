8d91c6a7ced3a8fc648fc637a55627b9a83b846b
OleksiyZ <106480026+OleksiyZ@users.noreply.github.com>
2023-04-28 12:16:51 +0300

    виправив маршрут і відповідь конфлікт

--- controlers/auth/createUser.js
+++ controlers/auth/createUser.js
@@ -5,7 +5,7 @@ const createUser = async (req, res) => {
 	const { email, password } = req.body;
 	const registeredUser = await User.findOne({ email });
 	if (registeredUser) {
-		throw res.status(409);
+		throw res.status(409).json({ message: "Email in use" });
 	}
 	const hashPassword = await bcrypt.hash(password, 10);
 	const newUser = await User.create({ ...req.body, password: hashPassword });
